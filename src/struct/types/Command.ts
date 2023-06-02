ecbe096151502678cebf4bf5af93256cca75b45b
vinybergamo <vinybergamo@gmail.com>
2023-06-02 18:39:56 -0300

    feat: create command instance

---
+++ src/struct/types/Command.ts
@@ -0,0 +1,56 @@
+import {
+  ApplicationCommandData,
+  ButtonInteraction,
+  Collection,
+  CommandInteraction,
+  CommandInteractionOptionResolver,
+  ModalSubmitInteraction,
+  RoleSelectMenuInteraction,
+  StringSelectMenuInteraction,
+} from "discord.js";
+import { Client } from "../client";
+
+interface CommandProps {
+  client: Client;
+  interaction: CommandInteraction;
+  options: CommandInteractionOptionResolver;
+}
+
+export type ComponentsButton = Collection<
+  string,
+  (interaction: ButtonInteraction) => any
+>;
+
+export type ComponentsStringSelecMenu = Collection<
+  string,
+  (interaction: StringSelectMenuInteraction) => any
+>;
+
+export type ComponentsRolesSelecMenu = Collection<
+  string,
+  (interaction: RoleSelectMenuInteraction) => any
+>;
+
+export type ComponentsModal = Collection<
+  string,
+  (interaction: ModalSubmitInteraction) => any
+>;
+
+interface CommandComponents {
+  buttons: ComponentsButton;
+  selects: ComponentsStringSelecMenu;
+  rolesSelects: ComponentsRolesSelecMenu;
+  modals: ComponentsModal;
+}
+
+export type CommandType = ApplicationCommandData &
+  CommandComponents & {
+    exec(interaction: CommandProps): any;
+  };
+
+export class Command {
+  constructor(options: CommandType) {
+    options.dmPermission = false;
+    Object.assign(this, options);
+  }
+}
